c26cf47e2337154bd0ba7bc7d860f3a1cbca101e
Yi He <yih@yih-thinkpadt14gen4.nay.csb>
2024-03-29 13:52:01 +0800

    init

---
+++ tmt/tests/t1/t1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "This is test case t1"
+cat /etc/os-release
+uname -m
+exit 0
+
---
+++ tmt/tests/t2/t2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "This is test case t2"
+cat /etc/os-release
+uname -m
+exit 0
+
