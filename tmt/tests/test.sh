c2d40cd496f8ac63180a1018be9e8b90e76bc6df
Yi He <yih@yih-thinkpadt14gen4.nay.csb>
2024-04-02 20:43:33 +0800

    edge

--- tmt/tests/test.sh
+++ tmt/tests/test.sh
@@ -5,8 +5,28 @@ cd ../../ || exit 1
 function run_tests() {
 	if [ "$TEST_CASE" = "edge-commit" ]; then
 		./ostree.sh
+	elif [ "$TEST_CASE" = "edge-installer" ]; then
+		./ostree-installer.sh
 	elif [ "$TEST_CASE" = "edge-raw-image" ]; then
 		./ostree-raw-image.sh
+	elif [ "$TEST_CASE" = "edge-ami-image" ]; then
+		./ostree-ami-image.sh
+	elif [ "$TEST_CASE" = "edge-simplified-installer" ]; then
+		./ostree-simplified-installer.sh
+	elif [ "$TEST_CASE" = "edge-vsphere" ]; then
+		./ostree-vsphere.sh
+	elif [ "$TEST_CASE" = "edge-fdo-aio" ]; then
+		./ostree-fdo-aio.sh
+	elif [ "$TEST_CASE" = "edge-ignition" ]; then
+		./ostree-ignition.sh
+	elif [ "$TEST_CASE" = "edge-pulp" ]; then
+		./ostree-pulp.sh
+	elif [ "$TEST_CASE" = "edge-minimal" ]; then
+		./minimal-raw.sh
+	elif [ "$TEST_CASE" = "edge-8to9" ]; then
+		./ostree-8-to-9.sh
+	elif [ "$TEST_CASE" = "edge-9to9" ]; then
+		./ostree-9-to-9.sh
 	else
 		echo "Error: Test case $TEST_CASE not found!"
 		exit 1
